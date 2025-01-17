6ff5fd303a55339c222a465a6676c0c1b6b12d3b
jigong2024 <choijming21@naver.com>
2025-01-17 18:22:43 +0900

    test: 투두리스트 테스트 컴포넌트 생성 및 tanstack query devTools 이용해 캐싱 확인 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import TodoTest from "./components/todo/TodoTest";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <TodoTest />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
