377565216396b7ca00cc94f8c960b854df6aa445
jigong2024 <choijming21@naver.com>
2025-01-17 23:15:00 +0900

    feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import TodoTest from "./components/todo/TodoTest";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import SignIn from "./pages/auth/Signin";
+import SignUp from "./pages/auth/Signup";
+import MyPage from "./pages/auth/MyPage";
+import Home from "./pages/Home";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <TodoTest />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </BrowserRouter>
+
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import TodoTest from "../components/todo/TodoTest";
+
+const Home = () => {
+  return (
+    <div>
+      <TodoTest />
+    </div>
+  );
+};
+
+export default Home;
