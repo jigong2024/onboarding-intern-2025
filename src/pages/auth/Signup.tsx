544c65b64613bfcb6f268421a22d9bcbd2ba1e2e
jigong2024 <choijming21@naver.com>
2025-01-19 22:13:22 +0900

    edit: 파일 이름 변경

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -12,7 +12,7 @@ const SignUp = () => {
   useEffect(() => {
     if (token) {
       setTimeout(() => {
-        alert("이미 로그인된 상태입니다.");
+        alert("이미 로그인 된 상태입니다.");
         navigate("/");
       }, 0);
     }
